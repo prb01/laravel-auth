cfb397d54f79abdc7fddd5eba63adfe7cd7411d7
prb01 <p.bergstroem@gmail.com>
2022-12-16 16:49:26 -0300

    finished section 8: authentication

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    @auth
+                        Hi, {{ Auth::user()->name }}
+                    @else
+                        Laravel
+                    @endauth
                 </div>
 
                 <div class="links">
